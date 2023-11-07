7635c5163c78bae86d5cdba7db0c14e98f2b0fbe
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-11-06 20:18:43 -0600

    FETCH STUDIO

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,28 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function() {
+
+    const container = document.getElementById('container');
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+
+            for (let i = 0; i < data.length; i++) {
+                let astronaut = data[i];
+                container.innerHTML += `
+                <div class="astronaut">
+    <div class="bio">
+        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+        <ul>
+        <li>Hours in space: ${astronaut.hoursInSpace}</li>
+        <li>Active: ${astronaut.active}</li>
+        <li>Skills: ${astronaut.skills.join(", ")}</li>
+        </ul>
+    </div>
+    <img class="avatar" src="${astronaut.picture}">
+</div>
+    `;
+            }
+        });
+    });
+});
